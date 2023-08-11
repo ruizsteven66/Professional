06578bcbe923ef19a2abbbbdd2559d13d93cdf98
ruizsteven66 <141885071+ruizsteven66@users.noreply.github.com>
2023-08-10 18:56:29 -0700

    Add files via upload
    A simple MadLib.

---
+++ MadLib/MadLib/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadLib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a Noun :");
+            string NounOne = Console.ReadLine();
+            
+            Console.Write("Please enter a Prepostion :");
+            string Preposition = Console.ReadLine();
+            
+            Console.Write("Please enter a adjective :");
+            string adjectiveOne = Console.ReadLine();
+            
+            Console.Write("Please enter another Noun :");
+            string NounTwo = Console.ReadLine();
+
+            Console.Write("Please enter another adjective :");
+            string adjectiveTwo = Console.ReadLine();
+
+
+            Console.Write($"A {NounOne} was walking {Preposition} the street. A big { adjectiveOne} {NounTwo} was coming the other direction.The moon was {adjectiveTwo}.");
+
+
+            Console.ReadKey();
+        
+        }
+
+    }
+}
