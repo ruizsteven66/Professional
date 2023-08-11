cde6365358d6789a60b8751d56b5419d2b7f380c
ruizsteven66 <141885071+ruizsteven66@users.noreply.github.com>
2023-08-10 18:55:37 -0700

    Add files via upload
    just a very simplistic paint form with one color(Crimson).

---
+++ PainterForm/PainterForm/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PainterForm
+{
+    public partial class Form1 : Form
+    {
+        //using the decision of clicking the mouse button to paint.
+        bool ShouldPaint { get; set; } = false;
+        
+        //default constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ShouldPaint = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            ShouldPaint = false;
+        }
+
+        //Drawing a circle whenever mouse moves with its button down.
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (ShouldPaint) 
+            {
+                // a Using statement for Graphics object.
+                using (Graphics graphics = CreateGraphics())
+                {
+                    graphics.FillEllipse(new SolidBrush(Color.Crimson), e.X, e.Y, 10, 10);
+                    
+                }
+
+            }
+        }
+    }
+}
